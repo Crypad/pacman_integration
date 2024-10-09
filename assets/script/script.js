8fe0044e5b45ec711789fc4f79e3895a81de62b1
Crypad <erwanncrevel.ec@gmail.com>
2024-10-09 16:10:30 +0200

    2nd commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,8 @@ let pacman = document.getElementById("pacman");
 let cherry = document.getElementById("cherry");
 let scoreSpan = document.getElementById("scoreSpan");
 let ghost1 = document.getElementById("ghost1");
+let ghost2 = document.getElementById("ghost2");
+
 let cherryX = 0;
 let cherryY = 0;
 let score = 0
@@ -26,21 +28,37 @@ let differenceY = 0;
 let randomNumberY = 0;
 let randomValueY = 0;
 
-// Variables ghost
+// Variables ghost1
 let ghost1X = 0;
 let ghost1Y = 0;
 
-let minGX = 0;
-let maxGX = 0;
-let differenceGX = 0;
-let randomNumberGX = 0;
-let randomValueGX = 0;
+let minG1X = 0;
+let maxG1X = 0;
+let differenceG1X = 0;
+let randomNumberG1X = 0;
+let randomValueG1X = 0;
 
-let minGY = 0;
-let maxGY = 0;
-let differenceGY = 0;
-let randomNumberGY = 0;
-let randomValueGY = 0;
+let minG1Y = 0;
+let maxG1Y = 0;
+let differenceG1Y = 0;
+let randomNumberG1Y = 0;
+let randomValueG1Y = 0;
+
+// Variable ghost2
+let ghost2X = 0;
+let ghost2Y = 0;
+
+let minG2X = 0;
+let maxG2X = 0;
+let differenceG2X = 0;
+let randomNumberG2X = 0;
+let randomValueG2X = 0;
+
+let minG2Y = 0;
+let maxG2Y = 0;
+let differenceG2Y = 0;
+let randomNumberG2Y = 0;
+let randomValueG2Y = 0;
 
 let randomDirection = 0;
 
@@ -89,7 +107,7 @@ function checkCherry() {
     }
 }
 
-// Fonction pour faire apparaitre le fantome aléatoirement
+// Fonction pour faire apparaitre les fantomes aléatoirement
 function randomGhost1Start() {
     minG1X = 0;
     maxG1X = Math.floor(viewport.clientWidth / 50); // Divide viewport width by 50
@@ -114,10 +132,36 @@ function randomGhost1Start() {
     }
 }
 
+function randomGhost2Start() {
+    minG2X = 0;
+    maxG2X = Math.floor(viewport.clientWidth / 50); // Divide viewport width by 50
+    differenceG2X = maxG2X - minG2X + 1;
+    randomNumberG2X = Math.floor(Math.random() * differenceG2X);
+    randomValueG2X = randomNumberG2X * 50;
+    ghost2X = randomValueG2X;
+    if (ghost2X !== 0) {
+        ghost2X -= 50;
+    }
+
+    minG2Y = 0;
+    maxG2Y = Math.floor(viewport.clientHeight / 50); // Divide viewport height by 50
+    differenceG2Y = maxG2Y - minG2Y + 1;
+    randomNumberG2Y = Math.floor(Math.random() * differenceG2Y);
+    randomValueG2Y = randomNumberG2Y * 50;
+    ghost2Y = randomValueG2Y;
+    if (ghost2Y === 0 || ghost2Y === 50) {
+        ghost2Y += 100;
+    } else {
+        ghost2Y -= 100;
+    }
+}
+
+// FONCTION GENERATION D'UN NOMBRE ALEATOIRE ENTRE 1 ET 4
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// FONCTION MOUVEMENT DU FANTOME
 function moveGhost1() {
     randomDirection = getRandomInt(1, 4);
     // 1 = left / 2 = up / 3 = right / 4 = down
@@ -155,8 +199,53 @@ function moveGhost1() {
     } 
 }
 
+function moveGhost2() {
+    randomDirection = getRandomInt(1, 4);
+    // 1 = left / 2 = up / 3 = right / 4 = down
+    if (randomDirection === 1) {
+        if (ghost2X <= 0) {
+            ghost2X += 150;  
+        } else {
+            ghost2X -= 100;
+        }
+        ghost2.style.transform = `translateX(${ghost2X}px) translateY(${ghost2Y}px) scaleX(1)`;
+    }
+    if (randomDirection === 2) {
+        if (ghost2Y <= 0) {
+            ghost2Y += 150;  
+        } else {
+            ghost2Y -= 100;
+        }
+        ghost2.style.transform = `translateX(${ghost2X}px) translateY(${ghost2Y}px)`;
+    }
+    if (randomDirection === 3) {
+        if (ghost2X >= viewport.clientWidth) {
+            ghost2X -= 200;  
+        } else {
+            ghost2X += 100;
+        }
+        ghost2.style.transform = `translateX(${ghost2X}px) translateY(${ghost2Y}px) scaleX(-1)`;
+    }
+    if (randomDirection === 4) {
+        if (ghost2Y >= viewport.clientHeight) {
+            ghost2Y -= 200;  
+        } else {
+            ghost2Y += 100;
+        }
+        ghost2.style.transform = `translateX(${ghost2X}px) translateY(${ghost2Y}px)`;
+    } 
+}
+
+// FONCTION DE VERIFICATION DES COLLISIONS
 function checkCollisionG1() {
     if (pacmanX === ghost1X && pacmanY === ghost1Y) {
+        gameOver.style.transform = "scaleY(100%)";
+        console.log("COLLISION HAPPENED");
+    }
+}
+
+function checkCollisionG2() {
+    if (pacmanX === ghost2X && pacmanY === ghost2Y) {
         gameOver.style.transform = "scaleY(100%)";
         console.log("COLLISION HAPPENED");
     }
@@ -177,8 +266,10 @@ function movePacman(direction) {
             }
             checkCherry();
             moveGhost1();
+            moveGhost2();
             pacman.style.transform = "translate(" + pacmanX + "px," + pacmanY + "px) rotate(180deg)"
             checkCollisionG1();
+            checkCollisionG2();
             break;
 
         case "haut":
@@ -187,8 +278,10 @@ function movePacman(direction) {
             }
             checkCherry();
             moveGhost1();
+            moveGhost2();
             pacman.style.transform = "translate(" + pacmanX + "px," + pacmanY + "px) rotate(270deg)"
             checkCollisionG1();
+            checkCollisionG2();
             break;
 
         case "droite":
@@ -197,8 +290,10 @@ function movePacman(direction) {
             }
             checkCherry();
             moveGhost1();
+            moveGhost2();
             pacman.style.transform = "translate(" + pacmanX + "px," + pacmanY + "px) rotate(0deg)"
             checkCollisionG1();
+            checkCollisionG2();
             break;
 
         case "bas":
@@ -207,14 +302,17 @@ function movePacman(direction) {
             }
             checkCherry();
             moveGhost1();
+            moveGhost2();
             pacman.style.transform = "translate(" + pacmanX + "px," + pacmanY + "px) rotate(90deg)"
             checkCollisionG1();
+            checkCollisionG2();
             break;
 
         default:
             break;
     }
-    console.log("GHOST","Pos X:" + ghost1X, "Pos Y:" + ghost1Y);
+    console.log("GHOST2","Pos X:" + ghost2X, "Pos Y:" + ghost2Y);
+    console.log("GHOST1","Pos X:" + ghost1X, "Pos Y:" + ghost1Y);
     console.log("PACMAN","Pos X:" + pacmanX, "Pos Y:" + pacmanY);
 }
 // ----------------------------------------------------------------------
@@ -222,7 +320,8 @@ function movePacman(direction) {
 // Initialisation de la position du fantome
 randomGhost1Start()
 ghost1.style.transform = `translateX(${ghost1X}px) translateY(${ghost1Y}px)`;
-
+randomGhost2Start()
+ghost2.style.transform = `translateX(${ghost2X}px) translateY(${ghost2Y}px)`;
 
 // Initialisation de la position de la cerise
 randomCherryX();
